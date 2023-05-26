6e22cac14c75388ed77a8a34cee2854df10268bc
abdulazizpro378 <abdujazizabdi045@gmail.com>
2023-05-27 03:07:45 +0500

    izoh

--- js/home.js
+++ js/home.js
@@ -1,5 +1,4 @@
-
-var btnToggle = document.getElementById("toggle");
+// var btnToggle = document.getElementById("toggle");
 const dropdown = document.querySelector(".dropdown");
 const open_btn = document.querySelector(".open-modal");
 const close_btn = document.querySelector(".close-modal");
@@ -10,42 +9,51 @@ let btn2 = document.getElementById("btn-2");
 let btn3 = document.getElementById("btn-3");
 let btn4 = document.getElementById("btn-4");
 let iframe = document.querySelector(".iframe-box");
+let card = document.querySelector(".cards");
+let catd_noviy = document.querySelector(".noviy");
+let magazin = document.querySelector(".magazin");
+let hero_card = document.querySelector(".hero");
+let status_card = document.querySelector(".status_row");
+
+//drobdown
 
 function toggle() {
   dropdown.classList.toggle("d-blok");
 }
 
-btnToggle.addEventListener("click", toggle);
+// drobdown-end
 
+// modal show
 
-  function modalShow() {
-    modal.classList.add("modal-show");
-    modal_content.classList.add("modal-content-show");
+function modalShow() {
+  modal.classList.add("modal-show");
+  modal_content.classList.add("modal-content-show");
+}
+
+function modalHide() {
+  modal.classList.remove("modal-show");
+  modal_content.classList.remove("modal-content-show");
+}
+
+open_btn.addEventListener("click", modalShow);
+
+close_btn.addEventListener("click", modalHide);
+
+window.addEventListener("click", function (e) {
+  if (e.target === modal) {
+    modalHide();
   }
-  
-  function modalHide() {
-    modal.classList.remove("modal-show");
-    modal_content.classList.remove("modal-content-show");
+});
+
+window.addEventListener("keydown", function (e) {
+  if (e.key == "Escape") {
+    modalHide();
   }
-  
-  open_btn.addEventListener("click", modalShow);
-  
-  close_btn.addEventListener("click", modalHide);
-  
-  window.addEventListener("click", function (e) {
-    if (e.target === modal) {
-      modalHide();
-    }
-  });
-  
-  window.addEventListener("keydown", function (e) {
-    if (e.key == "Escape") {
-      modalHide();
-    }
-  });
+});
 
+// modal show end
 
-
+// map start
 btn2.addEventListener("click", function () {
   this.classList.add("active");
   iframe.innerHTML = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3038.9420216292137!2d71.78567611546045!3d40.38797757936857!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38bb83a471c6b55b%3A0x9927f2609db0d333!2sNajot%20Ta&#39;lim%20Farg&#39;ona!5e0!3m2!1sen!2s!4v1679488781708!5m2!1sen!2s"width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`;
@@ -77,3 +85,183 @@ btn4.addEventListener("click", function () {
   btn3.classList.remove("active");
   btn2.classList.remove("active");
 });
+
+// map-end
+
+// aksiya start
+
+function getCard({ img, name, name_price, desc, rating }) {
+  return `
+  <div class="card">
+              <div class="card-top">
+                <img src="${img}" alt="img" />
+                <span class="sale">-50%</span>
+                <span class="material-symbols-outlined heart"> favorite </span>
+              </div>
+              <div class="card-bottom">
+                <div class="price-pay">
+                  <div class="price">
+                    <p class="first-price">${name}</p>
+                    <p class="second-price">${name_price}</p>
+                  </div>
+  
+                  <div class="pay">
+                    <p class="with-card">С картой</p>
+                    <p class="simple">Обычная</p>
+                  </div>
+                </div>
+  
+                <div class="card-info">
+                  <p>
+                    ${desc}
+                  </p>
+                </div>
+  
+                <div class="card-star">
+                ${"★".repeat(rating) + "☆".repeat(5 - rating)}
+                </div>
+  
+                <div class="card-btn">
+                  <button><a href="#">В корзину</a></button>
+                </div>
+              </div>
+            </div>
+  `;
+}
+
+search_card_products.sort((a, b) => a.id - b.id);
+
+search_card_products
+  .filter((el) => el.discount != 0)
+  .slice(-4)
+  .forEach((el) => {
+    card.innerHTML += getCard(el);
+  });
+
+// aksiya end
+
+// noviy start
+function getNoviy({ name, img, desc, rating }) {
+  return `
+  <div class="card">
+              <div class="card-top">
+                <img src="${img}" alt="img" />
+  
+                <span class="material-symbols-outlined heart"> favorite </span>
+              </div>
+              <div class="card-bottom">
+                <div class="price-pay">
+                  <div class="price">
+                    <p class="first-price price-2">${name}</p>
+                  </div>
+                </div>
+  
+                <div class="card-info">
+                  <p>
+                    ${desc}
+                  </p>
+                </div>
+  
+                <div class="card-star">
+                ${"★".repeat(rating) + "☆".repeat(5 - rating)}
+                </div>
+  
+                <div class="card-btn">
+                  <button><a href="#">В корзину</a></button>
+                </div>
+              </div>
+            </div>
+  `;
+}
+
+search_card_products
+  .filter((el) => el.id != -1)
+  .slice(0, 4)
+  .forEach((el) => {
+    catd_noviy.innerHTML += getNoviy(el);
+  });
+
+// noviy end
+
+// Наши магазины start
+
+function getMagazin({ name, img, desc, rating }) {
+  return `
+    <div class="card">
+                <div class="card-top">
+                  <img src="${img}" alt="img" />
+    
+                  <span class="material-symbols-outlined heart"> favorite </span>
+                </div>
+                <div class="card-bottom">
+                  <div class="price-pay">
+                    <div class="price">
+                      <p class="first-price price-2">${name}</p>
+                    </div>
+                  </div>
+    
+                  <div class="card-info">
+                    <p>
+                      ${desc}
+                    </p>
+                  </div>
+    
+                  <div class="card-star">
+                  ${"★".repeat(rating) + "☆".repeat(5 - rating)}
+                  </div>
+    
+                  <div class="card-btn">
+                    <button><a href="#">В корзину</a></button>
+                  </div>
+                </div>
+              </div>
+    `;
+}
+
+search_card_products
+  .filter((el) => el.id != -1)
+  .slice(0, 4)
+  .forEach((el) => {
+    magazin.innerHTML += getMagazin(el);
+  });
+
+// hero
+
+function getHero({ desc, name_price, img }) {
+  return `
+  <div class="hero_card and1">
+              <div class="hero_card_content">
+                <h4>${desc}</h4>
+                <p>${name_price}</p>
+              </div>
+            <img src="${img}" alt="">
+    </div>  
+  `;
+}
+
+massiv2
+  .filter((el) => el.id != -1)
+  .forEach((el) => {
+    hero_card.innerHTML += getHero(el);
+  });
+
+// hero end
+
+// status start
+
+function getStatus({ img, name_price, desc }) {
+  return `
+    <div class="status_card">
+    <img src="${img}" alt="res">
+    <h3>${name_price}</h3>
+    <p>${desc}</p>
+      <button class="btn">Подробнее</button>
+  </div>
+    `;
+}
+
+massiv33
+  .filter((el) => el.id != -1)
+  .forEach((el) => {
+    status_card.innerHTML += getStatus(el);
+  });
---
+++ js/product.js
@@ -0,0 +1,532 @@
+// ------
+
+const search_card_products = [
+  {
+    id: 0,
+
+    fullike: "images/yurak-bosh.png",
+    like: "images/yurak-qizil.png",
+    img: "images/noviy-1.png",
+    name: "599,99 ₽",
+    name_price: "50.50 ₽",
+    desc: "Сосиски вареные МЯСНАЯ ИСТОРИЯ Молочные и С сыро...",
+    rating: 3,
+    discount: 4,
+    isLiked: false,
+  },
+  {
+    id: 1,
+
+    fullike: "images/yurak-bosh.png",
+    like: "images/yurak-qizil.png",
+    img: "images/moloko.png",
+    name: "44.50 ₽",
+    name_price: "50.50 ₽",
+    desc: "Молоко ПРОСТОКВАШИНО паст. питьевое цельное отборное...",
+    rating: 3,
+    discount: 1,
+    isLiked: false,
+  },
+  {
+    id: 2,
+
+    fullike: "images/yurak-bosh.png",
+    like: "images/yurak-qizil.png",
+    img: "images/moloko.png",
+    name: "159,99 ₽",
+    name_price: "50.50 ₽",
+    desc: "Колбаса сырокопченая МЯСНАЯ ИСТОРИЯ Сальчичон и Тоскан...",
+    rating: 4,
+    discount: 5,
+    isLiked: false,
+  },
+  {
+    id: 3,
+
+    fullike: "images/yurak-bosh.png",
+    like: "images/yurak-qizil.png",
+    img: "images/card-4-sosiska.png",
+    name: "49.39 ₽",
+    name_price: "50.50 ₽",
+    desc: "Сосиски вареные МЯСНАЯ ИСТОРИЯ Молочные и С сыро...",
+    rating: 2,
+    discount: 3,
+    isLiked: false,
+  },
+  {
+    id: 4,
+
+    fullike: "images/yurak-bosh.png",
+    like: "images/yurak-qizil.png",
+    img: "images/result-img14.png",
+    name: "77,99 ₽",
+    name_price: "50.50 ₽",
+    desc: "Сосиски вареные МЯСНАЯ ИСТОРИЯ Молочные и С сыро...",
+    rating: 2,
+
+    isLiked: false,
+  },
+  {
+    id: 5,
+
+    fullike: "images/yurak-bosh.png",
+    like: "images/yurak-qizil.png",
+    img: "images/result-img3.png",
+    name: "159,99 ₽",
+    name_price: "50.50 ₽",
+    desc: "Молоко ПРОСТОКВАШИНО паст. питьевое цельное отборное...",
+    rating: 3,
+
+    isLiked: false,
+  },
+  {
+    id: 6,
+
+    fullike: "images/yurak-bosh.png",
+    like: "images/yurak-qizil.png",
+    img: "images/result-img15.png",
+    name: "599,99 ₽",
+    name_price: "50.50 ₽",
+    desc: "Колбаса сырокопченая МЯСНАЯ ИСТОРИЯ Сальчичон и Тоскан...",
+    rating: 5,
+
+    isLiked: false,
+  },
+  {
+    id: 7,
+
+    fullike: "images/yurak-bosh.png",
+    like: "images/yurak-qizil.png",
+    img: "images/result-img2.png",
+    name: "49,39 ₽",
+    name_price: "50.50 ₽",
+    desc: "Сосиски вареные МЯСНАЯ ИСТОРИЯ Молочные и С сыро...",
+    rating: 4,
+
+    isLiked: false,
+  },
+  {
+    id: 8,
+
+    fullike: "images/yurak-bosh.png",
+    like: "images/yurak-qizil.png",
+    img: "images/result-img2.png",
+    name: "44.50 ₽",
+    name_price: "50.50 ₽",
+    desc: "Сосиски вареные МЯСНАЯ ИСТОРИЯ Молочные и С сыро...",
+    rating: 2,
+
+    isLiked: false,
+  },
+  {
+    id: 9,
+
+    fullike: "images/yurak-bosh.png",
+    like: "images/yurak-qizil.png",
+    img: "images/result-img3.png",
+    name: "44.50 ₽",
+    name_price: "50.50 ₽",
+    desc: "Молоко ПРОСТОКВАШИНО паст. питьевое цельное отборное...",
+    rating: 3,
+
+    isLiked: false,
+  },
+  {
+    id: 10,
+    fullike: "images/yurak-bosh.png",
+    like: "images/yurak-qizil.png",
+    img: "images/result-img3.png",
+    name: "44.50 ₽",
+    name_price: "50.50 ₽",
+    desc: "Колбаса сырокопченая МЯСНАЯ ИСТОРИЯ Сальчичон и Тоскан...",
+    rating: 5,
+
+    isLiked: false,
+  },
+  {
+    id: 11,
+    fullike: "images/yurak-bosh.png",
+    like: "images/yurak-qizil.png",
+    img: "images/result-img2.png",
+    name: "44.50 ₽",
+    name_price: "50.50 ₽",
+    desc: "Сосиски вареные МЯСНАЯ ИСТОРИЯ Молочные и С сыро...",
+    rating: 4,
+
+    isLiked: false,
+  },
+
+  {
+    id: 13,
+    fullike: "images/yurak-bosh.png",
+    like: "images/yurak-qizil.png",
+    img: "images/result-img2.png",
+    name: "44.50 ₽",
+    name_price: "50.50 ₽",
+    desc: "Молоко ПРОСТОКВАШИНО паст. питьевое цельное отборное...",
+    rating: 3,
+
+    isLiked: false,
+  },
+  {
+    id: 14,
+    fullike: "images/yurak-bosh.png",
+    like: "images/yurak-qizil.png",
+    img: "images/result-img3.png",
+    name: "44.50 ₽",
+    name_price: "50.50 ₽",
+    desc: "Колбаса сырокопченая МЯСНАЯ ИСТОРИЯ Сальчичон и Тоскан...",
+    rating: 4.5,
+
+    isLiked: false,
+  },
+  {
+    id: 15,
+    fullike: "images/yurak-bosh.png",
+    like: "images/yurak-qizil.png",
+    img: "images/result-img3.png",
+    name: "44.50 ₽",
+    name_price: "50.50 ₽",
+    desc: "Сосиски вареные МЯСНАЯ ИСТОРИЯ Молочные и С сыро...",
+    rating: 3.5,
+
+    isLiked: false,
+  },
+  {
+    id: 16,
+    fullike: "images/yurak-bosh.png",
+    like: "images/yurak-qizil.png",
+    img: "images/result-img15.png",
+    name: "599,99 ₽",
+    name_price: "50.50 ₽",
+    desc: "Сосиски вареные МЯСНАЯ ИСТОРИЯ Молочные и С сыро...",
+    rating: 3.5,
+
+    isLiked: false,
+  },
+  {
+    id: 17,
+    fullike: "images/yurak-bosh.png",
+    like: "images/yurak-qizil.png",
+    img: "images/result-img3.png",
+    name: "44.50 ₽",
+    name_price: "50.50 ₽",
+    desc: "Молоко ПРОСТОКВАШИНО паст. питьевое цельное отборное...",
+    rating: 3,
+
+    isLiked: false,
+  },
+  {
+    id: 18,
+    fullike: "images/yurak-bosh.png",
+    like: "images/yurak-qizil.png",
+    img: "images/result-img3.png",
+    name: "159,99 ₽",
+    name_price: "50.50 ₽",
+    desc: "Колбаса сырокопченая МЯСНАЯ ИСТОРИЯ Сальчичон и Тоскан...",
+    rating: 5,
+
+    isLiked: false,
+  },
+  {
+    id: 19,
+    fullike: "images/yurak-bosh.png",
+    like: "images/yurak-qizil.png",
+    img: "images/result-img2.png",
+    name: "49.39 ₽",
+    name_price: "50.50 ₽",
+    desc: "Сосиски вареные МЯСНАЯ ИСТОРИЯ Молочные и С сыро...",
+    rating: 4,
+
+    isLiked: false,
+  },
+  {
+    id: 20,
+    fullike: "images/yurak-bosh.png",
+    like: "images/yurak-qizil.png",
+    img: "images/result-img14.png",
+    name: "77,99 ₽",
+    name_price: "50.50 ₽",
+    desc: "Сосиски вареные МЯСНАЯ ИСТОРИЯ Молочные и С сыро...",
+    rating: 2,
+
+    isLiked: false,
+  },
+  {
+    id: 21,
+    fullike: "images/yurak-bosh.png",
+    like: "images/yurak-qizil.png",
+    img: "images/result-img3.png",
+    name: "159,99 ₽",
+    name_price: "50.50 ₽",
+    desc: "Молоко ПРОСТОКВАШИНО паст. питьевое цельное отборное...",
+    rating: 3,
+
+    isLiked: false,
+  },
+  {
+    id: 22,
+    fullike: "images/yurak-bosh.png",
+    like: "images/yurak-qizil.png",
+    img: "images/result-img15.png",
+    name: "599,99 ₽",
+    name_price: "50.50 ₽",
+    desc: "Колбаса сырокопченая МЯСНАЯ ИСТОРИЯ Сальчичон и Тоскан...",
+    rating: 5,
+
+    isLiked: false,
+  },
+  {
+    id: 23,
+    fullike: "images/yurak-bosh.png",
+    like: "images/yurak-qizil.png",
+    img: "images/result-img2.png",
+    name: "49,39 ₽",
+    name_price: "50.50 ₽",
+    desc: "Сосиски вареные МЯСНАЯ ИСТОРИЯ Молочные и С сыро...",
+    rating: 4,
+
+    isLiked: false,
+  },
+  {
+    id: 24,
+    fullike: "images/yurak-bosh.png",
+    like: "images/yurak-qizil.png",
+    img: "images/result-img2.png",
+    name: "44.50 ₽",
+    name_price: "50.50 ₽",
+    desc: "Сосиски вареные МЯСНАЯ ИСТОРИЯ Молочные и С сыро...",
+    rating: 2,
+
+    isLiked: false,
+  },
+  {
+    id: 25,
+    fullike: "images/yurak-bosh.png",
+    like: "images/yurak-qizil.png",
+    img: "images/result-img3.png",
+    name: "44.50 ₽",
+    name_price: "50.50 ₽",
+    desc: "Молоко ПРОСТОКВАШИНО паст. питьевое цельное отборное...",
+    rating: 3,
+
+    isLiked: false,
+  },
+  {
+    id: 26,
+    fullike: "images/yurak-bosh.png",
+    like: "images/yurak-qizil.png",
+    img: "images/result-img3.png",
+    name: "44.50 ₽",
+    name_price: "50.50 ₽",
+    desc: "Колбаса сырокопченая МЯСНАЯ ИСТОРИЯ Сальчичон и Тоскан...",
+    rating: 5,
+
+    isLiked: false,
+  },
+  {
+    id: 27,
+    fullike: "images/yurak-bosh.png",
+    like: "images/yurak-qizil.png",
+    img: "images/moloko.png",
+    name: "44.50 ₽",
+    name_price: "50.50 ₽",
+    desc: "Сосиски вареные МЯСНАЯ ИСТОРИЯ Молочные и С сыро...",
+    rating: 4,
+
+    isLiked: false,
+  },
+
+  {
+    id: 29,
+    fullike: "images/yurak-bosh.png",
+    like: "images/yurak-qizil.png",
+    img: "images/moloko.png",
+    name: "44.50 ₽",
+    name_price: "50.50 ₽",
+    desc: "Молоко ПРОСТОКВАШИНО паст. питьевое цельное отборное...",
+    rating: 3,
+
+    isLiked: false,
+  },
+  {
+    id: 30,
+    fullike: "images/yurak-bosh.png",
+    like: "images/yurak-qizil.png",
+    img: "images/image.png",
+    name: "44.50 ₽",
+    name_price: "50.50 ₽",
+    desc: "Колбаса сырокопченая МЯСНАЯ ИСТОРИЯ Сальчичон и Тоскан...",
+    rating: 4.5,
+
+    isLiked: false,
+  },
+  {
+    id: 31,
+    fullike: "images/yurak-bosh.png",
+    like: "images/image.png",
+    img: "images/card-3-kolbasa.svg",
+    name: "44.50 ₽",
+    name_price: "50.50 ₽",
+    desc: "Сосиски вареные МЯСНАЯ ИСТОРИЯ Молочные и С сыро...",
+    rating: 3.5,
+    descount: "",
+    isLiked: false,
+  },
+];
+
+const massiv2 = [
+  {
+    id: 0,
+    img: "images/result-img22.png",
+    name: "599,99 ₽",
+    name_price: "И получайте бонусы при покупке в магазинах и на сайте",
+    desc: "Оформите карту «Северяночка» ",
+    rating: 3.5,
+
+    isLiked: false,
+  },
+  {
+    id: 1,
+    img: "images/result-img23.png",
+    name: "44.50 ₽",
+    name_price: "И получайте вдвое больше бонусов",
+    desc: "Покупайте акционные товары",
+    rating: 3,
+    isLiked: false,
+  },
+];
+
+const massiv33 = [
+  {
+    id: 0,
+    img: "images/result-img25.png",
+    name: "599,99 ₽",
+    name_price: "Режим использования масок и перчаток на территории магазинов",
+    desc: "Подробная информация о режимах использования масок и перчаток на территории магазинов ЛЕНТА. Информация обновляется каждый будний день.",
+    rating: 3.5,
+    isLiked: false,
+  },
+  {
+    id: 1,
+    img: "images/status-img26.png",
+    name: "44.50 ₽",
+    name_price: "Весеннее настроение для каждой",
+    desc: "8 Марта – это не просто Международный женский день, это ещё день тюльпанов, приятных сюрпризов и праздничных тёплых пожеланий taviling milk a foote deleta man spoon",
+    rating: 3,
+    isLiked: false,
+  },
+  {
+    id: 3,
+    img: "images/result-img27.png",
+    name: "599,99 ₽",
+    name_price: "ЗОЖ или ФАСТФУД. А вы на чьей стороне? Голосуем!",
+    desc: "Голосуйте за любимые категории, выбирайте категорию-победителя в мобильном приложении и получайте кешбэк 10% баллами в апреле!",
+    rating: 3.5,
+    isLiked: false,
+  },
+];
+
+const massiv11 = [
+  {
+    name: "157,50 $",
+    img: "images/kow.svg",
+  },
+  {
+    name: "331,50 $",
+    img: "images/cow2.svg",
+  },
+  {
+    name: "154,50 $",
+    img: "images/cow3.svg",
+  },
+  {
+    name: "157,50 $",
+    img: "images/kow.svg",
+  },
+];
+
+const massiv34 = [
+  {
+    id: 0,
+    img: "images/catigori1.png",
+    name: "Молоко, сыр, яйцо",
+  },
+  {
+    id: 3,
+    img: "images/catigori2.png",
+    name: "Хлеб",
+  },
+  {
+    id: 3,
+    img: "images/catigori3.png",
+    name: "Фрукты и овощи",
+  },
+  {
+    id: 3,
+    img: "images/catigori4.png",
+    name: "Напитки",
+  },
+  {
+    id: 3,
+    img: "images/catigori3.png",
+    name: "Кондитерские изделия",
+  },
+  {
+    id: 3,
+    img: "images/catigori6.png",
+    name: "Чай, кофе",
+  },
+
+  {
+    id: 3,
+    img: "images/catigori7.png",
+    name: "Бакалея",
+  },
+  {
+    id: 3,
+    img: "images/catigori8.png",
+    name: "Здоровое питание",
+  },
+  {
+    id: 3,
+    img: "images/catigori9.png",
+    name: "Зоотовары",
+  },
+  {
+    id: 3,
+    img: "images/catigori3.png",
+    name: "Фрукты и овощи",
+  },
+  {
+    id: 3,
+    img: "images/catigori4.png",
+    name: "Напитки",
+  },
+  {
+    id: 3,
+    img: "images/catigori3.png",
+    name: "Кондитерские изделия",
+  },
+  {
+    id: 3,
+    img: "images/catigori6.png",
+    name: "Чай, кофе",
+  },
+
+  {
+    id: 3,
+    img: "images/catigori7.png",
+    name: "Бакалея",
+  },
+  {
+    id: 3,
+    img: "images/catigori8.png",
+    name: "Здоровое питание",
+  },
+  {
+    id: 3,
+    img: "images/catigori9.png",
+    name: "Зоотовары",
+  },
+];
